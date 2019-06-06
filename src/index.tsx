c47c406cfc2683b254adc37adeff255a8add4c76
Matt <matteopieroni6@gmail.com>
2019-06-06 23:06:49 +0100

    Init App
    Added dependencies

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+  return (
+    <div>Hello My Friend!</div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
