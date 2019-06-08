3ee27f7616dd1c415e139d8dc3cf63500d62b378
Matt <matteopieroni6@gmail.com>
2019-06-08 17:52:13 +0100

    Updated circleci

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -1,17 +1,22 @@
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
+require('dotenv').config();
 
 var config = {
-  username: process.env.FTPUSERNAME,
+  user: process.env.FTPUSERNAME,
   password: process.env.FTPPASS,
   host: process.env.FTPHOST,
   port: 21,
   localRoot: __dirname + '/../dist/',
-  remoteRoot: '/',
+  remoteRoot: '/public_html/',
   include: ['*'],
 };
 
+console.log(config);
+
 ftpDeploy.deploy(config, function(err) {
-  if (err) throw new Error(err);
-  else console.log('finished');
+  if (err) {
+    throw new Error(err);
+    process.exit(1);
+  } else console.log('finished');
 });
