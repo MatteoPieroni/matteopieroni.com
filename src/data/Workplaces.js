680668c09dfa5266ca8d5e9724f82ff95e124a47
Matteo <matteopieroni6@gmail.com>
2020-07-29 20:23:27 +0100

    Updated work content

--- src/data/Workplaces.js
+++ src/data/Workplaces.js
@@ -1,10 +1,26 @@
 export const Workplaces = [
   {
+    name: 'Zone Digital',
+    dates: '2019 - Present',
+    place: 'London',
+    description:
+      'At Zone, my role is Front End Developer, working mainly with a client, a few apps that will be used by healthcare systems and companies to request and carry out genomic testing for patients.\nThe technology used is state-of-the-art for React developers and the different applications have similar stacks though serving different purposes in the stages of genomic testing.',
+    tech:
+      'React, Redux, @testing-library, enzyme, Formik, react-hook-form, @reach/router, react-router-dom, Npm, GIT, Jenkins, Lerna, ATI Analytics',
+    projects: [
+      {
+        projectTitle: 'Analytics',
+        text:
+          'Our applications were using a custom implementation to send analytics data related to UX KPIs. As part of the work we wanted to do on it, we streamlined the solution for adding, updating, and sending the data. The new implementation was a success for the dev team and for the UX and DA teams.',
+      },
+    ],
+  },
+  {
     name: 'Abel & Cole',
     dates: '2018 - 2019',
     place: 'London',
     description:
-      'At Abel & Cole, my role is Front End Developer, working on the e-commerce web platform. The platform is based on an MVC .NET backend, that used Razor and jQuery. \n I have been part of the transition to modern architecture, mainly using React and Redux, and CI/CD pipeline on Azure.',
+      'At Abel & Cole, my role was Front End Developer, working on the e-commerce web platform. The platform is based on an MVC .NET backend, that used Razor and jQuery.\nI have been part of the transition to modern architecture, mainly using React and Redux, and CI/CD pipeline on Azure.',
     tech:
       'React, Redux, jQuery, Lodash, Node, Npm, Browserify, Babel, Less, Bem (methodology), Bootstrap,Pa11y, Google Lighthouse, React Native, GIT, Azure Pipelines, Agile (methodology)',
     projects: [
@@ -35,7 +51,7 @@ export const Workplaces = [
     dates: '2016 - 2018',
     place: 'Lucca (IT)',
     description:
-      'At Memphremagog my role was Front End Developer, working on the client’s web projects. The job consisted of the realisation of websites, from the initial brief all the way to production. \n I worked alongside the designers, copywriters, and social media managers.',
+      'At Memphremagog my role was Front End Developer, working on the client’s web projects. The job consisted of the realisation of websites, from the initial brief all the way to production.\nI worked alongside the designers, copywriters, and social media managers.',
     tech:
       'Sass, Grunt, Foundation, Bootstrap, jQuery, Wordpress, Woocommerce, Drupal 8, MODX, Illustrator, Photoshop, Adobe Animate',
     listOfLinks: [
@@ -74,19 +90,19 @@ export const Workplaces = [
     dates: '2011-2018',
     place: 'Lucca (IT)',
     description:
-      'At Pieroni srl, from Marketing Manager, I was quickly drawn into the world of websites. \n During my time of employment, we went through 2 major redesigns of the website, to improve its use for customers and the value returned to the business.',
+      'At Pieroni srl, from Marketing Manager, I was quickly drawn into the world of websites.\nDuring my time of employment, we went through 2 major redesigns of the website, to improve its use for customers and the value returned to the business.',
     tech:
       'AngularJS, jQuery, Node, Npm, Sass, Foundation, MaterializeCSS, Wordpress, Woocommerce, Joomla, Foundation for emails, BeeFree, Mailchimp, GIT, Firebase Database and Storage, Amazon AWS EC2, Illustrator, Photoshop, Buffer',
     projects: [
       {
         projectTitle: 'Pieroni.it 3.0',
         text:
-          'As a trial for a future e-commerce website, we decided to rebuild the platform using Woocommerce. \n I designed and developed the whole website, in collaboration with a copywriter and an agency for SEO',
+          'As a trial for a future e-commerce website, we decided to rebuild the platform using Woocommerce.\nI designed and developed the whole website, in collaboration with a copywriter and an agency for SEO',
       },
       {
         projectTitle: 'Intranet Portal',
         text:
-          'I created the company intranet portal, using AngularJS with the backend in Firebase Realtime Db and Storage, with features like the ability to send an SMS to customers (through a 3rd party API), and to calculate the cost of delivery with our trucks (through Google Maps Places and Distance APIs). \n For the administrators, the portal would also show the visits of the main website (using Google Analytics), the activity in our Facebook Workplace groups, the chronology of the communications sent via SMS, and a utility to create already branded communication posters (using Node).',
+          'I created the company intranet portal, using AngularJS with the backend in Firebase Realtime Db and Storage, with features like the ability to send an SMS to customers (through a 3rd party API), and to calculate the cost of delivery with our trucks (through Google Maps Places and Distance APIs).\nFor the administrators, the portal would also show the visits of the main website (using Google Analytics), the activity in our Facebook Workplace groups, the chronology of the communications sent via SMS, and a utility to create already branded communication posters (using Node).',
       },
       {
         projectTitle: 'Website performance optimisation',
