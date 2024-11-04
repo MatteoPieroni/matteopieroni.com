e9ee72cbb1f4abc8c5d7ca960068b864e1ccc488
Matteo Pieroni <matteopieroni6@gmail.com>
2024-11-04 01:07:05 +0000

    feature: replace with astro

---
+++ src/data/workplaces.ts
@@ -0,0 +1,135 @@
+export const workplaces = [
+	{
+		name: 'Zoopla',
+		dates: '2021 - Present',
+		place: 'London',
+		description:
+			'At Zone, my role is Front End Developer, working mainly with a client, a few apps that will be used by healthcare systems and companies to request and carry out genomic testing for patients.\nThe technology used is state-of-the-art for React developers and the different applications have similar stacks though serving different purposes in the stages of genomic testing.',
+		tech:
+			'React, Redux, @testing-library, enzyme, Formik, react-hook-form, @reach/router, react-router-dom, Npm, GIT, Jenkins, Lerna, ATI Analytics',
+		projects: [
+			{
+				projectTitle: 'Analytics',
+				text:
+					'Our applications were using a custom implementation to send analytics data related to UX KPIs. As part of the work we wanted to do on it, we streamlined the solution for adding, updating, and sending the data. The new implementation was a success for the dev team and for the UX and DA teams.',
+			},
+		],
+	},
+	{
+		name: 'Zone Digital',
+		dates: '2019 - 2021',
+		place: 'London',
+		description:
+			'At Zone, my role is Front End Developer, working mainly with a client, a few apps that will be used by healthcare systems and companies to request and carry out genomic testing for patients.\nThe technology used is state-of-the-art for React developers and the different applications have similar stacks though serving different purposes in the stages of genomic testing.',
+		tech:
+			'React, Redux, @testing-library, enzyme, Formik, react-hook-form, @reach/router, react-router-dom, Npm, GIT, Jenkins, Lerna, ATI Analytics',
+		projects: [
+			{
+				projectTitle: 'Analytics',
+				text:
+					'Our applications were using a custom implementation to send analytics data related to UX KPIs. As part of the work we wanted to do on it, we streamlined the solution for adding, updating, and sending the data. The new implementation was a success for the dev team and for the UX and DA teams.',
+			},
+		],
+	},
+	{
+		name: 'Abel & Cole',
+		dates: '2018 - 2019',
+		place: 'London',
+		description:
+			'At Abel & Cole, my role was Front End Developer, working on the e-commerce web platform. The platform is based on an MVC .NET backend, that used Razor and jQuery.\nI have been part of the transition to modern architecture, mainly using React and Redux, and CI/CD pipeline on Azure.',
+		tech:
+			'React, Redux, jQuery, Lodash, Node, Npm, Browserify, Babel, Less, Bem (methodology), Bootstrap,Pa11y, Google Lighthouse, React Native, GIT, Azure Pipelines, Agile (methodology)',
+		projects: [
+			{
+				projectTitle: 'Performance Audit',
+				text:
+					'After a performance audit, I devised and worked on optimisation, which resulted in a satisfying gain, especially in Time To First-Paint.',
+			},
+			{
+				projectTitle: 'Accessibility Improvements',
+				text:
+					'I analysed the existing accessibility of the website and set myself as A11y champion, writing and changing code to use the best tools of the languages, to provide the best experience for all users.I also created and published an Npm package called Acest, to be added to the pipeline for testing the accessibility of the deployed websites.',
+			},
+			{
+				projectTitle: 'Introduction of Jest',
+				text:
+					'I introduced Front End unit tests using Jest as the testing framework, to test vanilla JavaScript and React/Redux code. In React I also introduced the use of Enzyme.',
+			},
+			{
+				projectTitle: 'POC of App',
+				text:
+					'I created a React Native application, cooperating with a Back End engineer for the API/GraphQL side.The application gives our internal staff the ability to buy items in our in-house shop.',
+			},
+		],
+	},
+	{
+		name: 'Memphremagog Digital Media Factory',
+		dates: '2016 - 2018',
+		place: 'Lucca (IT)',
+		description:
+			'At Memphremagog my role was Front End Developer, working on the client’s web projects. The job consisted of the realisation of websites, from the initial brief all the way to production.\nI worked alongside the designers, copywriters, and social media managers.',
+		tech:
+			'Sass, Grunt, Foundation, Bootstrap, jQuery, Wordpress, Woocommerce, Drupal 8, MODX, Illustrator, Photoshop, Adobe Animate',
+		listOfLinks: [
+			{
+				link: 'http://www.lalocandafrancigena.it',
+				image: 'website-locanda-francigena-matteo-pieroni.jpg',
+			},
+			{
+				link: 'http://www.memphremagog.it',
+				image: 'website-memphremagog-matteo-pieroni.jpg',
+			},
+			{
+				link: 'http://madeinlucca.it',
+				image: 'website-made-in-lucca-matteo-pieroni.jpg',
+			},
+			{
+				link: 'https://www.miabamabag.it',
+				image: 'website-mia-bama-bag-matteo-pieroni.jpg',
+			},
+			{
+				link: 'http://www.tecnoservice.lucca.it',
+				image: 'website-tecnoservice-lucca-matteo-pieroni.jpg',
+			},
+			{
+				link: 'http://www.rctabletop.it',
+				image: 'website-rctt-matteo-pieroni.jpg',
+			},
+			{
+				link: 'http://www.eventservicetuscany.it',
+				image: 'website-est-matteo-pieroni.jpg',
+			},
+		],
+	},
+	{
+		name: 'Pieroni srl',
+		dates: '2011-2018',
+		place: 'Lucca (IT)',
+		description:
+			'At Pieroni srl, from Marketing Manager, I was quickly drawn into the world of websites.\nDuring my time of employment, we went through 2 major redesigns of the website, to improve its use for customers and the value returned to the business.',
+		tech:
+			'AngularJS, jQuery, Node, Npm, Sass, Foundation, MaterializeCSS, Wordpress, Woocommerce, Joomla, Foundation for emails, BeeFree, Mailchimp, GIT, Firebase Database and Storage, Amazon AWS EC2, Illustrator, Photoshop, Buffer',
+		projects: [
+			{
+				projectTitle: 'Pieroni.it 3.0',
+				text:
+					'As a trial for a future e-commerce website, we decided to rebuild the platform using Woocommerce.\nI designed and developed the whole website, in collaboration with a copywriter and an agency for SEO',
+			},
+			{
+				projectTitle: 'Intranet Portal',
+				text:
+					'I created the company intranet portal, using AngularJS with the backend in Firebase Realtime Db and Storage, with features like the ability to send an SMS to customers (through a 3rd party API), and to calculate the cost of delivery with our trucks (through Google Maps Places and Distance APIs).\nFor the administrators, the portal would also show the visits of the main website (using Google Analytics), the activity in our Facebook Workplace groups, the chronology of the communications sent via SMS, and a utility to create already branded communication posters (using Node).',
+			},
+			{
+				projectTitle: 'Website performance optimisation',
+				text:
+					'To optimise website’s performance, I worked on caching and assets optimisation of the Wordpress backend and frontend, which resulted in a score of 92 on Google Speed Insights.',
+			},
+			{
+				projectTitle: 'Website SEO optimisation',
+				text:
+					'I worked with an SEO agency to make sure every iteration of our website was fully optimised for SEO, specifically for Local SEO. This work consisted in improving the speed of the website, optimisation of the assets, the connection between all sections, and the use of Schema.org specific markup.',
+			},
+		],
+	},
+];
