907ea8dbc709237d96c8762bb6526065f8405dac
Matteo Pieroni <matteopieroni6@gmail.com>
2024-11-09 01:14:50 +0000

    chore: update pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   jsxA11y.flatConfigs.strict,
-  prettierPlugin,
 ];
