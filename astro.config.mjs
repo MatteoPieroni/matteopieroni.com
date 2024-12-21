8c3a00d13e94c5e41054bc1766ed0d04873a6d30
Matteo Pieroni <matteopieroni6@gmail.com>
2024-12-21 14:52:33 +0000

    chore: SEO sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://matteopieroni.com/',
+  integrations: [mdx(), sitemap()],
+  redirects: {
+    '/material-animation.html': '/'
+  }
 });
